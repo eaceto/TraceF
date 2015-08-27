02e62d1d39987a074c4fb2bf7e706f13ecfd2175
Kimi <ezequiel.aceto@gmail.com>
2015-08-27 16:43:06 -0300

    added enable/disable tracer

--- tracer.go
+++ tracer.go
@@ -12,6 +12,23 @@ import (
 	"fmt"
 )
 
+var traceEnabled bool
+
+// trace is disbled by default
+func init() {
+	traceEnabled = false
+}
+
+// Enabled returns true if the tracer is enabled
+func Enabled() (bool) {
+	return traceEnabled
+}
+
+// SetEnable enable or disable the trace
+func SetEnabled (enable bool) {
+	traceEnabled = enable
+}
+
 // Trace to stdout the entering to a function call
 func Trace(s string) ( io.Writer,  string,  time.Time )   {
 	return TraceToWriter(os.Stdout,s)
@@ -20,6 +37,11 @@ func Trace(s string) ( io.Writer,  string,  time.Time )   {
 // Trace to a writer the entering to a function call
 func TraceToWriter(writer io.Writer, as string) ( io.Writer, string,  time.Time )   {
 	now := time.Now()
+
+	if traceEnabled == false {
+		return nil, "", now
+	}
+
 	str := fmt.Sprint("[",now,"]\t-> ", as, "\n")
 	io.WriteString(writer, str)
 	return writer, as, now
@@ -27,6 +49,10 @@ func TraceToWriter(writer io.Writer, as string) ( io.Writer, string,  time.Time 
 
 // Trace to a writer the leaving to a function call
 func Un(writer io.Writer, as string, timestamp time.Time ) {
+	if traceEnabled == false || writer == nil {
+		return
+	}
+
 	now := time.Now()
 	sub := now.Sub(timestamp).Nanoseconds()/1000
 	str := fmt.Sprint("[",now,"]\t<- ", as, "\t\tspent:",  sub ,"ms\n")
--- tracer_test.go
+++ tracer_test.go
@@ -6,8 +6,23 @@ import (
 	"fmt"
 )
 
-func TestEnteringTrace(t *testing.T) {
-	defer tracer.Un(tracer.Trace("ExampleEnteringTrace"))
-	fmt.Println("\tthis should go between Trace and Un(trace) messages")
+func TestIsEnabled (t *testing.T) {
+	if tracer.Enabled() == true {
+		t.Errorf("Tracer should be disabled by default.")
+	}
 }
+
+func TestTrace (t *testing.T) {
+	tracer.SetEnabled(true)
+	defer tracer.Un(tracer.Trace("TestTrace"))
+	fmt.Println("\tthis should be between Trace and Un(trace) messages")
+}
+
+func TestTraceDisabled (t *testing.T) {
+	tracer.SetEnabled(false)
+	defer tracer.Un(tracer.Trace("TestTraceDisabled"))
+	fmt.Println("\tno trace message should be printed on Stdout")
+}
+
+
 
