1a4605631b085057a337e3ecd764316d4b7529d6
Kimi <ezequiel.aceto@gmail.com>
2015-08-27 16:09:48 -0300

    better example

--- tracer_test.go
+++ tracer_test.go
@@ -3,9 +3,11 @@ package tracer_test
 import (
 	"github.com/eaceto/tracer"
 	"testing"
+	"fmt"
 )
 
 func TestEnteringTrace(t *testing.T) {
-	tracer.Un(tracer.Trace("ExampleEnteringTrace"))
+	defer tracer.Un(tracer.Trace("ExampleEnteringTrace"))
+	fmt.Println("\tthis should go between Trace and Un(trace) messages")
 }
 
